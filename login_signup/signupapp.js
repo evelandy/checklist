a5a223cc4b102e6043038e5aed7d4aa619d22900
William Griffin <40611326+evelandy@users.noreply.github.com>
2019-08-11 16:06:29 -0500

    Update signupapp.js

--- login_signup/signupapp.js
+++ login_signup/signupapp.js
@@ -10,7 +10,7 @@ function addUser(e) {
     // put token in like this
     let token = localStorage.getItem('x-access-token');
 
-    fetch('http://checklist.ml/api/user', {
+    fetch('http://98.198.145.46/api/user', {
         method: 'POST',
         headers: {
             'Accept': 'application/json, text/plain, */*',
